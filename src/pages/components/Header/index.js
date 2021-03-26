548319e6847a451f0c55cc8574af2281c4678707
emersonpessoa01 <eps011108@gmail.com>
2021-03-25 21:16:08 -0300

    inserting colors in Header

--- src/pages/components/Header/index.js
+++ src/pages/components/Header/index.js
@@ -1,11 +1,21 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { LinearGradient } from "expo-linear-gradient";
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native';
+import  {LinearGradient} from "expo-linear-gradient"
+
 
 export default function Header() {
   return (
-    <LinearGradient >
-      <Text>14/01/2021</Text>
+    <LinearGradient style={styles.header}
+    colors={["#1ef6ff", "#97c1ff"]}
+    >
+      <Text style={styles.data}> data</Text>
+      <Text style={styles.city}> Belém</Text>
     </LinearGradient>
-  );
+  )
 }
+
+const styles = StyleSheet.create({
+  header: {
+    
+  }
+})
