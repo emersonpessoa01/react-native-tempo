6231a91eb3ccb00c420d3fa41ed133cde683c575
emersonpessoa01 <eps011108@gmail.com>
2021-04-07 15:45:04 -0300

    updated Search

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,10 +1,42 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+import { Feather } from "@expo/vector-icons";
 
 export default function Search() {
+  const [input, setInput] = useState("");
   return (
-    <View>
-      <Text>Página search</Text>
-    </View>
-  )
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity style={styles.backButton}>
+        <Feather name="chevron-left" size={32} color="#000" />
+        <Text style={{ fontSize: 22 }}>Voltar</Text>
+      </TouchableOpacity>
+
+      <View style={styles.searchBox}>
+        <TextInput
+          value={input}
+          onChange={(value) => setInput(value)}
+          placeholder="Ex:Belém, PA"
+          style={styles.input}
+
+        />
+
+        <TouchableOpacity style={styles.icon}>
+          <Feather 
+            name="seach"
+            color="#fff"
+            size={22}
+          />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({});
